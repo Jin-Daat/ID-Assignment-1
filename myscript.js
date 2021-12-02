4a0830822ddffe7da18922b4c59f0d3dfa87cc07
Jin Daat <s10222867@connect.np.edu.sg>
2021-12-02 23:01:19 +0800

    Changed html links in all files

--- myscript.js
+++ myscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 =========================================================
 Javscript for index.html
@@ -62,69 +61,4 @@ window.addEventListener("scroll", () =>{
       opacity = 0.5 + currentScroll/1000;
     }else{ opacity = 1; }
     document.querySelector(".skillsAwards").style.opacity = opacity;
-=======
-/*
-=========================================================
-Javscript for index.html
-=========================================================
-*/
-window.addEventListener("scroll", () =>{
-    var currentScroll = window.pageYOffset;
-    if (currentScroll <= 300){
-      opacity = 0 + currentScroll/300;
-    }else{ opacity = 1; }
-    document.querySelector(".contactSection").style.opacity = opacity;
-  })  
-
-//This function alerts the user after submitting the form and resets input 
-function SubmitForm() {
-    alert("Thank you for submitting the form! I will reach out to you soon! :)");
-    }    
-
-/*
-=========================================================
-Javscript for About Me - Personality page
-=========================================================
-*/
-window.addEventListener("scroll", () =>{
-    var currentScroll = window.pageYOffset;
-    if (currentScroll <= 300){
-      opacity = 0 + currentScroll/300;
-    }else{ opacity = 1; }
-    document.querySelector(".hobbies").style.opacity = opacity;
-  })
-
-/*
-=========================================================
-Javscript for About Me - Education page
-=========================================================
-*/
-window.addEventListener("scroll", () =>{
-    var currentScroll = window.pageYOffset;
-    if (currentScroll <= 300){
-      opacity = 0.5 + currentScroll/300;
-    }else{ opacity = 1; }
-    document.getElementById("element2").style.opacity = opacity;
-  })  
-
-  window.addEventListener("scroll", () =>{
-    var currentScroll = window.pageYOffset;
-    if (currentScroll <= 1300){
-      opacity = 0.35 + currentScroll/1300;
-    }else{ opacity = 1; }
-    document.getElementById("element3").style.opacity = opacity;
-  })
-
-/*
-=========================================================
-Javscript for About Me - Work & Skills page
-=========================================================
-*/
-window.addEventListener("scroll", () =>{
-    var currentScroll = window.pageYOffset;
-    if (currentScroll <= 1000){
-      opacity = 0.5 + currentScroll/1000;
-    }else{ opacity = 1; }
-    document.querySelector(".skillsAwards").style.opacity = opacity;
->>>>>>> 8117c79030e12b84e298febfeedbd9d76f24a4c0
   })
